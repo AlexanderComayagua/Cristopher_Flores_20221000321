d0380d37ed598a556a69d70c272e3c02adc56723
AlexanderComayagua <78757169+AlexanderComayagua@users.noreply.github.com>
2022-10-03 14:33:08 -0600

    Add files via upload

---
+++ ProgramacionII/src/Cristopher_Flores_Cap_4/Ejercicio4_21.java
@@ -0,0 +1,27 @@
+package Cristopher_Flores_Cap_4;
+import java.util.Scanner;
+public class Ejercicio4_21 {
+    public static void main(String args[]) {
+         Scanner z=new Scanner(System.in);
+        
+         int numero;
+         int contador=1;
+         int mayor;
+        
+         System.out.printf("\nIngrese un valor entero %d: ", contador++);
+         mayor=z.nextInt();
+
+         while (10>=contador){
+
+             System.out.printf("\nIngrese un valor entero %d: ", contador++);
+             numero=z.nextInt();
+
+             if (numero>mayor){
+                mayor=numero;
+             }
+         }
+
+         System.out.printf("\nNúmero mayor: %d\n", mayor);
+         
+    }
+}
---
+++ ProgramacionII/src/Cristopher_Flores_Cap_4/Ejercicio4_35.java
@@ -0,0 +1,28 @@
+package Cristopher_Flores_Cap_4;
+import java.util.Scanner;
+public class Ejercicio4_35 {
+    public static void main(String args[]) {
+         Scanner z=new Scanner(System.in);
+        
+         System.out.println("Lado 1: ");
+         int a=z.nextInt();
+         System.out.println("Lado 2: ");
+         int b=z.nextInt();
+         System.out.println("Lado 3: ");
+         int c=z.nextInt();
+         
+         if (a!=0 && b!=0 && c!=0){
+             if (a+b>c && a+c>b && c+b>a){
+                  System.out.printf("\n%d > %d",a+b,c);
+                  System.out.printf("\n%d > %d",a+c,b);
+                  System.out.printf("\n%d > %d\n",c+b,c);
+                  System.out.println("Los parámetros ingresados conforman las medidas de un triángulo");
+             }else{
+                  System.out.printf("\n%d > %d",a+b,c);
+                  System.out.printf("\n%d > %d",a+c,b);
+                  System.out.printf("\n%d > %d\n",c+b,c);
+                  System.out.println("Los parámetros ingresados no conforman las medidas de un triángulo");
+             }
+         }
+    }
+}
