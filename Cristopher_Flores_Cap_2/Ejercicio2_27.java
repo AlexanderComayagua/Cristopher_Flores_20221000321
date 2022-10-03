dea68339ef2a7051f074105ad811b57deeca947d
AlexanderComayagua <78757169+AlexanderComayagua@users.noreply.github.com>
2022-10-03 14:35:11 -0600

    Add files via upload

---
+++ Cristopher_Flores_Cap_2/Ejercicio2_27.java
@@ -0,0 +1,13 @@
+package Cristopher_Flores_Cap_2;
+public class Ejercicio2_27 {
+    public static void main(String args[]) {
+         System.out.println(" * * * * * * * *");
+         System.out.println("  * * * * * * * *");
+         System.out.println(" * * * * * * * *");
+         System.out.println("  * * * * * * * *");
+         System.out.println(" * * * * * * * *");
+         System.out.println("  * * * * * * * *");
+         System.out.println(" * * * * * * * *");
+         System.out.println("  * * * * * * * *");
+    }
+}
---
+++ Cristopher_Flores_Cap_5/Ejercicio5_11.java
@@ -0,0 +1,9 @@
+package Cristopher_Flores_Cap_5;
+import java.util.Scanner;
+public class Ejercicio5_11 {
+    public static void main(String args[]) {
+        Scanner z=new Scanner(System.in);
+        
+        
+    }
+}
